ab067b7591522d60a4560a6d9a6eb29a36243214
linarifux <linarifux@gmail.com>
2026-06-28 08:35:18 +0600

    updated app file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -86,7 +86,7 @@ export default function App() {
               path="/" 
               element={
                 <ProtectedRoute>
-                  <DashboardLayout />
+                   <DashboardLayout />
                 </ProtectedRoute>
               }
             >
