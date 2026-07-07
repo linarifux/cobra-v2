a1b496ba7d35d951dac14b5cb07745b76e9a5718
Naimul <linarifux@gmail.com>
2026-07-08 01:00:23 +0600

    godaddy domain added to the cors origin

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,12 @@ app.use(cors({
     "https://cobra-v2.vercel.app", 
     "https://cobra-v2.netlify.app", 
     "https://dsm-mi-orders.netlify.app",
-    "d1ymwhyqj02a47.cloudfront.net",
+    "https://d1ymwhyqj02a47.cloudfront.net",
     "http://98.83.145.156:5000",
-    "d3s3grz6mr9lfw.cloudfront.net"
+    "https://d3s3grz6mr9lfw.cloudfront.net",
+    "https://dsm.mi-orders.com",
+    "https://api.mi-orders.com",
+    "https://mi-orders.com",
   ],
   credentials: true 
 }));
